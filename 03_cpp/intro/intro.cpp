9b2c2b94ba9b993d56ff3f472e249d08a1628f62
yuuuuhang12 <zhaangyuuhaang5556@outlook.com>
2022-12-07 23:51:07 +0300

    разминка

---
+++ 03_cpp/intro/intro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "please enter your name:";
+  string username;
+  cin >> username; 
+  cout << "Hello," << username << "!\n";
+  return 0;  
+}
